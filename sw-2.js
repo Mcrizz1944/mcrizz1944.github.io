5d0f284024f4d9407a96b6ee8adb4067a3fa752e
Mcrizz1944 <mcallister_james@icloud.com>
2026-04-15 16:57:54 -0700

    Add files via upload

---
+++ sw-2.js
@@ -0,0 +1,36 @@
+const CACHE = 'liftlog-v2';
+const ASSETS = [
+  '/liftlog/',
+  '/liftlog/index.html',
+  '/liftlog/manifest.json',
+  '/liftlog/icon-192.png',
+  '/liftlog/icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(ASSETS).catch(function(){return;});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() {
+        return caches.match('/liftlog/index.html');
+      });
+    })
+  );
+});
